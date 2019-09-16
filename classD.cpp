4c15a934d8f30fda4d45af4963a5dd8aea16cad2
lilianasabato <liliana.teodoro@estudante.ufla.br>
2019-09-16 15:31:03 -0300

    Criacao da classe D

---
+++ classD.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+class D{
+	private:
+		string D1;
+		string D2;
+	public:
+		void MD1();
+		void MD2();		
+		
+};
+void D::MD1(){
+	cout<<"Criando metodo: MD1"<<endl;
+}
+void D::MD2(){
+	cout<<"Criando metodo: MD1"<<endl;
+}
